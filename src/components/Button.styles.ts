ae015fa6563706864596f91e8e05c9a8bb7bbabf
madalena-rocha <rochamada1997m@gmail.com>
2023-11-22 15:10:25 -0300

    configure colors and font

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme['green-500']};
   color: ${props => props.theme.white};
 
   // O styled components vai executar o código dentro do {} como uma função, e vai enviar para essa função todas as propriedades do ButtonContainer
