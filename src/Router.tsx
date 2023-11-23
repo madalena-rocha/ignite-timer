13396dbc3364d691eb160df96221e617e943e7e8
madalena-rocha <rochamada1997m@gmail.com>
2023-11-23 10:49:41 -0300

    route layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
