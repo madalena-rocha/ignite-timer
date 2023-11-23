930859d9c2714bdcf6e3c29cb72b6d714239d6cb
madalena-rocha <rochamada1997m@gmail.com>
2023-11-23 16:12:38 -0300

    History page structure and styling

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto; // se o tamanho da tabela for maior que o tamanho do container, vai gerar uma barra de rolagem
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse; // quando dois elementos vizinhos têm borda, conta como se só existisse uma borda entre eles
+    min-width: 600px; // forçar com que quando tiver um tamanho menor que 600px, gere o scroll
+
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
