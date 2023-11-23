404948648524d85a2369e502e6b36df20a6a9e3d
madalena-rocha <rochamada1997m@gmail.com>
2023-11-23 11:10:46 -0300

    layout styling

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />{' '}
       {/* Espaço para ser inserido o conteúdo específico da página */}
-    </div>
+    </LayoutContainer>
   )
 }
