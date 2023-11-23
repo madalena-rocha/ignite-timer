4f2a651ffa71939e3c6703efb912d4dfdd57f991
madalena-rocha <rochamada1997m@gmail.com>
2023-11-23 11:34:42 -0300

    finishing the Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+      // No hover, haverá uma borda inferior no link
+      // É necessário adicioná-la aqui para que o link não se mova no hover
+      // A borda superior foi adicionada para que o ícone fique centralizado
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
