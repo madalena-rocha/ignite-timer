6bf24c14ee71922331a030dd24acf9249559048a
madalena-rocha <rochamada1997m@gmail.com>
2023-11-25 14:24:36 -0300

    controlled vs uncontrolled components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+// import { useState } from 'react'
+
 import { Play } from 'phosphor-react'
 
 import {
@@ -10,16 +12,37 @@ import {
   TaskInput,
 } from './styles'
 
+// Existem dois modelos de trabalhar com formulários em React, controlled e uncontrolled components
+// Controlled é quando mantém em tempo real a informação inserida no input do usuário guardada dentro do estado no componente da aplicação
+// Benefícios: como tem o valor em tempo real, é possível facilmente ter acesso a esses valores no submit e refletir visualmente alterações na interface baseado no valor desses inputs
+// Desvantagens: no React, cada atualização de estado provoca uma nova renderização, recalculando todo o conteúdo do componente do estado que mudou, podendo se tornar um problema para a aplicação em questão de performance
+// Uncontrolled é quando busca a informação do valor do input somente quando precisa dela
+// Como não tem acesso ao valor digitado letra a letra, perde a fluidez de habilitar ou desabilitar coisas, mas ganha em performance
+// Formulários simples com poucos campos geralmente usam controlled, já dashboards com grande quantidade de inputs precisam usar o modelo de uncontrolled
+
 export function Home() {
+  // const [task, setTask] = useState('')
+
+  // function resetForm() {
+  //   setTask('')
+  // }
+
+  function handleSubmit(event) {
+    event.target.task.value
+  }
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
+            name="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            // onChange={(e) => setTask(e.target.value)}
+            // value={task} // se o estado mudar por uma origem que não seja a digitação do usuário, também atualizar visualmente esse input para mostrar o novo valor
           />
 
           <datalist id="task-suggestions">
@@ -51,7 +74,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
